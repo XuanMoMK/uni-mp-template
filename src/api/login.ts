abe6da546ed886a54a84fa93d7bc05f26c22e008
sunyu <2711783496@qq.com>
2023-11-18 11:06:32 +0800

    首页模块

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,5 +1,3 @@
-import { http } from '@/utils/http'
-
 /**
  * 小程序登录
  * @param data 请求参数
@@ -37,7 +35,7 @@ export const getMemberProfileAPI = () => {
 }
 
 /**
- * 修改个⼈信息
+ * 修改个人信息
  * @param data 请求体参数
  */
 export const putMemberProfileAPI = (data: ProfileParams) => {
